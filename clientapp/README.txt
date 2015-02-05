0e9c535a356f9889436e4ba3df06ec2634a0c1a2
kienvu58 <vtk5995@gmail.com>
2015-02-05 12:58:56 +0700

    use angular-websocket instead of ng-websocket

--- clientapp/README.txt
+++ clientapp/README.txt
@@ -1,15 +1,2 @@
 npm install -g bower
-bower install ng-websocket
-
-in: /www/lib/ng-websocket/ng-websocket.js:
-delete line 122 then add these lines:
-
-if (cfg.mock) {
-                me.$$ws = new $$mockWebsocket(cfg.mock, $http);
-            }
-            else if (cfg.protocols) {
-                me.$$ws = new WebSocket(cfg.url, cfg.protocols);
-            }
-            else {
-                me.$$ws = new WebSocket(cfg.url);
-            }
+bower install angular-websocket --save
--- clientapp/www/js/controllers.js
+++ clientapp/www/js/controllers.js
@@ -1,13 +1,11 @@
-'use strict';
-
-angular.module('start.controllers', ['ngWebsocket'])
+angular.module('start.controllers', ['angular-websocket'])
 
 .controller('appCtrl', function($scope, $websocket) {
   var self = this;
   self.ip = '127.0.0.1';
   self.port = '1234';
   self.time = 300;
-  self.task = 'shutdown';
+  self.task = 'hi';
   
 
   self.submit = function() {
@@ -19,22 +17,8 @@ angular.module('start.controllers', ['ngWebsocket'])
       return self.task+'|'+self.time.toString();
     };
 
-    var ws = $websocket.$new({
-      url: wsUrl(),
-      reconnect: false // set true then it will reconnect after 2 seconds
-    });
-
-    ws.$on('$open', function () {
-      console.log('Connected!');
-      ws.$$ws.send(request()); // send a message to the websocket server
-    });
-    ws.$on('$error', function() {
-      console.log('An error has occupied!');
-    });
-
-    ws.$on('$close', function () {
-      console.log('Disconnected!');
-    });
+    var ws = $websocket(wsUrl());
+    ws.send(request());
   };
 });
 
